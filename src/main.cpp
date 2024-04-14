726c6252823ed32dec14f74063d6f13d8fd46aed
DanVicenteIhanus <dan.vicente.ihanus@gmail.com>
2024-04-14 15:55:31 +0200

    testing mpi, seems to work

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,43 @@
-#include "ConvNet.hpp"
+//#include "../include/ConvNet.hpp"
 #include <mpi.h>
-
+#include <iostream>
 /* 
 
 TODO:
-
+  1. Write a regular backprop- of the convnet (no MPI)
+  2. Store the results
+  3. Need to create/ammend the cmake file for building?
+  4. 
 */
-int main() {
-  ConvNet net;
+int main(int argc, char* argv[]) {
+  //ConvNet net;
   
   // == Hyperparameters == //
-  const int64_t input_size = 784; // mnist size
-  const int64_t num_classes = 10;
-  const int64_t batch_size = 1; 
-  const size_t num_epochs = 5; 
+  const int input_size = 784; // mnist img-size (28^2)
+  const int num_classes = 10;
+  const int batch_size = 1; 
+  const int num_epochs = 5; 
   const double learning_rate = 0.001;
+  
+  // ================ //
+  //    MPI-setup     //
+  // ================ // 
+  int size, rank, ierr;
+  ierr = MPI_Init(&argc, &argv);
+  ierr = MPI_Comm_size(MPI_COMM_WORLD, &size);
+  ierr = MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
+  // testing mpi // looks like it works
+  std::cout << "Hi, this is process " << rank << ", whats up? \n";
+  /*
   const std::string MNIST_path = "../data/mnist/";
-  auto train_dataset = torch::data::datasets::MNIST(MNIST_path)
-    .map(torch::data::transforms::Normalize<>(0.1307, 0.3081))
-    .map(torch::data::transforms::Stack<>);
-  int num_train_samples = train_dataset.size().value();
+  auto train_dataset =
+      torch::data::datasets::MNIST(MNIST_path)
+          .map(torch::data::transforms::Normalize<>(0.1307, 0.3081))
+          .map(torch::data::transforms::Stack<>());
 
+  int num_train_samples = train_dataset.size().value();
+  */
+  ierr = MPI_Finalize();
   return 0;
 }
