6b7ad3ca953338c8f37e1e98bda15e5da606642e
DanVicenteIhanus <dan.vicente.ihanus@gmail.com>
2024-05-20 23:15:28 +0200

    clean up

--- src/ConvNet.cpp
+++ src/ConvNet.cpp
@@ -1,7 +1,3 @@
-/*
-CNN with 3 conv-layers and 1 pooling layer.
-Forward pass is implemented here and the backprop is implemented in "main.cpp"
-*/
 #include "../include/convnet.h"
 #include <torch/torch.h>
 
@@ -11,30 +7,24 @@ ConvNetImpl::ConvNetImpl(int64_t num_classes, int64_t num_channels) {
 		torch::nn::ReLU(),
 		torch::nn::MaxPool2d(torch::nn::MaxPool2dOptions(2).stride(2))
 	);
-
 	conv2 = torch::nn::Sequential(
 		torch::nn::Conv2d(torch::nn::Conv2dOptions(30, 40, 5).stride(1)),
 		torch::nn::ReLU(),
 		torch::nn::BatchNorm2d(40),
 		torch::nn::MaxPool2d(torch::nn::MaxPool2dOptions(2).stride(2))
 	);
-
 	fc1 = torch::nn::Sequential(
 		torch::nn::Linear(40*4*4, 700),
 		torch::nn::ReLU()
-			// torch::nn::Dropout2d(torch::nn::Dropout2dOptions(0.3)) // too much dropout?
 	);
-
 	fc2 = torch::nn::Sequential(
 		torch::nn::Linear(700, num_classes)
 	);
-
 	register_module("conv1", conv1);
 	register_module("conv2", conv2);
 	register_module("fc1", fc1);
 	register_module("fc2", fc2);
 }
-
 torch::Tensor ConvNetImpl::forward(torch::Tensor x) {
 	x = conv1->forward(x);
 	x = conv2->forward(x);
