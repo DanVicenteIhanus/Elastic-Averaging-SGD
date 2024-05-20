dad44d718543f4870283226a362e4e8686333341
elinde2 <elinde2@kth.se>
2024-05-20 18:55:38 +0200

    Plotting and results code

--- src/plotting.py
+++ src/plotting.py
@@ -117,11 +117,53 @@ def copyAllAlphaFiles():
         if re.search(r"alpha", filename):
             print(filename)
             shutil.copy(folderPath + filename, newFolderPath + filename)
+
+def threshholdTestAccuracy():
+    threshhold = 0.7
+    
+    path_CIFAR = "../results/cifar/"
+    path_CIFAR_MSGD = path_CIFAR + "msgd/stats_cifar_MSGD_delta_0.9.txt"
+    
+    df_benchmark = pd.read_csv(path_CIFAR_MSGD)
+    df_benchmark = addEpochColumnToDF(df_benchmark)
+    interpolated_epoch = getInterpolatedEpoch(threshhold,df_benchmark['Test_Accuracy'])
+    print("Interpolated epoch: ", interpolated_epoch)
+    
+    taus = np.array([2,4,8])
+    workers = np.array([2,4,8])
+    epochs = np.zeros((len(workers),len(taus)))
+    for i in range(len(taus)):
+        for j in range(len(workers)):
+            tau = taus[i]
+            worker = workers[j]
+            filename = "stats_cifar_EAMSGD_size" + str(worker + 1) + "_rank_0_tau_" + str(tau) + "_alpha_0.25_delta_0.9_momentum_0.txt"
+            df = pd.read_csv(path_CIFAR + "eamsgd/" + filename)
+            #print(df.columns)
+            interpolated_ep = getInterpolatedEpoch(threshhold,df['Testing_accuracy'])
+            epochs[j][i] = interpolated_ep
+    paralell_speedup = np.divide(interpolated_epoch,epochs)
+    parallel_efficiency = np.divide(paralell_speedup.T, workers+1).T
+    
+    print("Epochs (interpolated) to reach", threshhold, "test accuracy:")
+    print(epochs)
+    print("Parallel speedup")
+    print(paralell_speedup)
+    print("Parallel efficiency")
+    print(parallel_efficiency)
+    
+
+def getInterpolatedEpoch(threshhold,df_col):
+    idx = np.argmax(df_col >= threshhold)
+    acc_upper = df_col.iloc[idx]
+    acc_lower = df_col.iloc[idx-1]
+    frac_idx = (threshhold-acc_lower)/(acc_upper-acc_lower)
+    interpolated_idx = idx -1 + frac_idx
+    return interpolated_idx + 1
         
 def communicationRatios(folderPath):
     taus = [2,4,8]
     workers = [2,4,8]
-    communicationRatios = np.zeros((len(taus),len(workers)))
+    communicationRatios = np.zeros((len(workers),len(taus)))
     print(communicationRatios.shape)
     for i in range(len(taus)):
         for j in range(len(workers)):
@@ -135,7 +177,105 @@ def communicationRatios(folderPath):
                 communicationRatios[j,i] += 1/worker * df[' Total_comm_time'].iloc[-1]/df['Duration'].iloc[-1]
     
     return communicationRatios
+
+def plotAllSDG(plotPath,fontSize):
+    #Plot all four methods for CIFAR as function of duration
+    #Then CIFAR as function of epochs
+    #Also, MNIST as function of epochs
     
+    path_CIFAR = "../results/cifar/"
+    path_MNIST = "../results/mnist/"
+    
+    path_CIFAR_SGD = path_CIFAR + "msgd/stats_cifar_MSGD_delta_0.txt"
+    path_CIFAR_MSGD = path_CIFAR + "msgd/stats_cifar_MSGD_delta_0.9.txt"
+    path_CIFAR_EASGD = path_CIFAR + "easgd/stats_cifar_EASGD_size5_rank_0_tau_4_alpha_0.25.txt"
+    path_CIFAR_EAMSGD = path_CIFAR + "eamsgd/stats_cifar_EAMSGD_size5_rank_0_tau_4_alpha_0.25_delta_0.9_momentum_0.txt"
+    
+    path_MNIST_SGD = path_MNIST + "msgd/stats_mnist_MSGD_delta_0.txt"
+    path_MNIST_MSGD = path_MNIST + "msgd/stats_mnist_MSGD_delta_0.9.txt"
+    path_MNIST_EASGD = path_MNIST + "easgd/stats_mnist_EASGD_size5_rank_0_tau_4_alpha_0.25.txt"
+    path_MNIST_EAMSGD = path_MNIST + "eamsgd/stats_mnist_EAMSGD_size5_rank_0_tau_4_alpha_0.25_delta_0.9_momentum_0.txt"
+    
+    df_cifar_SGD = pd.read_csv(path_CIFAR_SGD)
+    df_cifar_MSGD = pd.read_csv(path_CIFAR_MSGD)
+    df_cifar_EASGD = pd.read_csv(path_CIFAR_EASGD)
+    df_cifar_EAMSGD = pd.read_csv(path_CIFAR_EAMSGD)
+    df_mnist_SGD = pd.read_csv(path_MNIST_SGD)
+    df_mnist_MSGD = pd.read_csv(path_MNIST_MSGD)
+    df_mnist_EASGD = pd.read_csv(path_MNIST_EASGD)
+    df_mnist_EAMSGD = pd.read_csv(path_MNIST_EAMSGD)
+    
+    dict_CIFAR = {
+        'SGD': df_cifar_SGD,
+        'MSGD': df_cifar_MSGD,
+        'EASGD': df_cifar_EASGD,
+        'EAMSGD': df_cifar_EAMSGD
+    }
+    dict_MNIST = {
+        'SGD': df_mnist_SGD,
+        'MSGD': df_mnist_MSGD,
+        'EASGD': df_mnist_EASGD,
+        'EAMSGD': df_mnist_EAMSGD
+    }
+    
+    for key in dict_CIFAR:
+        dict_CIFAR[key] = addEpochColumnToDF(dict_CIFAR[key])
+    
+    for key in dict_MNIST:
+        dict_MNIST[key] = addEpochColumnToDF(dict_MNIST[key])
+        print(dict_MNIST[key])
+    
+    colors = {
+        'SGD': ':*b',
+        'MSGD': ':^r',
+        'EASGD': ':vg',
+        'EAMSGD': ':sc'
+    }
+    #CIFAR over duration
+    plt.figure()
+    plt.rcParams.update({'font.size': fontSize})
+
+    for key in dict_CIFAR:
+        df = dict_CIFAR[key]
+        plt.plot(df['Duration'], df.iloc[:,2], colors[key], lw=0.5,label=key)
+    plt.legend()
+    plt.title("CIFAR: Test Accuracy vs. Wall-Clock Time")
+    plt.xlabel('Wall-clock time (s)', fontsize=fontSize)
+    plt.ylabel('Test accuracy', fontsize=fontSize)
+    plt.savefig(plotPath + "allSGD_CIFAR"+ "_duration.pdf", bbox_inches="tight")
+    plt.close()
+    
+    #CIFAR over epochs
+    plt.figure()
+    plt.rcParams.update({'font.size': fontSize})
+
+    for key in dict_CIFAR:
+        df = dict_CIFAR[key]
+        plt.plot(df['Epoch'], df.iloc[:,2], colors[key], lw=0.5,label=key)
+    plt.legend()
+    plt.title("CIFAR: Test Accuracy")
+    plt.xlabel('Number of Epochs', fontsize=fontSize)
+    plt.ylabel('Test accuracy', fontsize=fontSize)
+    plt.savefig(plotPath + "allSGD_CIFAR"+ "_epoch.pdf", bbox_inches="tight")
+    plt.close()
+    
+    #MNIST over epochs
+    plt.figure()
+    plt.rcParams.update({'font.size': fontSize})
+
+    for key in dict_MNIST:
+        df = dict_MNIST[key]
+        plt.plot(df['Epoch'], df.iloc[:,2], colors[key], lw=0.5,label=key)
+    plt.legend()
+    plt.title("MNIST: Test Accuracy")
+    plt.xlabel('Number of Epochs', fontsize=fontSize)
+    plt.ylabel('Test accuracy', fontsize=fontSize)
+    plt.savefig(plotPath + "allSGD_MNIST"+ "_epoch.pdf", bbox_inches="tight")
+    plt.close()
+
+def plotGridEASGD():
+    #same code pretty much as below
+    pass
 
 def plotGridEAMSGD(folderPath,plotPath, df_benchmark):
     taus = [2,4,8]
@@ -192,6 +332,13 @@ def plotGridEAMSGD(folderPath,plotPath, df_benchmark):
             plt.savefig(plotPath + fileTitle + "_test_accuracy.pdf", bbox_inches="tight")
             plt.close()
             
+def addEpochColumnToDF(df):
+    df['Duration'] = df['Duration']/1000
+    df = df.reset_index()
+    df = df.rename(columns={"index": "Epoch"})
+    df["Epoch"] = df["Epoch"] + 1
+    return df
+    
  
 fontSize = 15
 folder_path_CIFAR = "../results/CIFAR/"
@@ -239,11 +386,18 @@ folderPathEAMSGD = folder_path_CIFAR + "eamsgd/"
 # df_mnist = df_mnist.rename(columns={"index": "Epoch"})
 # df_mnist["Epoch"] = df_mnist["Epoch"] + 1
 
+
+
 #Time communication ratio 
-communicationRatioTable = communicationRatios(folderPathEAMSGD)
-print(communicationRatioTable*100)
+# communicationRatioTable = communicationRatios(folderPathEAMSGD)
+# print(communicationRatioTable*100)
+
+#Plot all SGD methods
+#plotAllSDG(plotPath, fontSize)
 
 #plotGridEAMSGD(folderPathEAMSGD,plotPath,df_cifar_MSGD)
+
+threshholdTestAccuracy()
 
 
 
